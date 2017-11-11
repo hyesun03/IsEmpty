db29b8993c456f700bd402313a471d4dcde001e2
함태영 <tae0code@gmail.com>
2017-11-12 07:25:29 +0900

    Alter Settings.py.  add Template dir & Login Redirect URL

--- booksite/settings.py
+++ booksite/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATES_DIR = (os.path.join(BASE_DIR, 'templates'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -126,3 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Custom User Model
 
 AUTH_USER_MODEL = 'accounts.User'
+
+#Login REDIRECT
+
+LOGIN_REDIRECT_URL = '/ok'
