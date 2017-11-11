b0ccabed64498434ddf21d30e7f5b448459289cf
함태영 <tae0code@gmail.com>
2017-11-12 07:27:19 +0900

    alter urls & views.py implement login ( using django auth form )

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
-
-from .views import LoginView, JoinView
+from django.contrib.auth import views as auth_views
+from .views import Signup, Signup_ok, Ok
 
 app_name = 'accounts'
 
 urlpatterns = [
-    url(r'^login/', LoginView.as_view()),
-    url(r'^join/', JoinView.as_view())
+    url(r'^ok/$', Ok.as_view()),
+    url(r'^login/$', auth_views.login, name='login_url'),
+    url(r'^logout/$', auth_views.logout, {'next_page' : '/login/'}),
+    url(r'^signup/$', Signup.as_view(), name='signup'),
+    url(r'^signup_ok/$', Signup_ok.as_view(), name='success_signup')
 ]
