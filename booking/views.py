ab0bbff9bf5c9a3547bb87f4f35a1c7387c25467
hyesun03 <chsun0303@gmail.com>
2017-11-19 13:43:44 +0900

    Add booking_delete template and view

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, View, FormView
+from django.views.generic import TemplateView, View, FormView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
+from django.utils import timezone
+from django.http import HttpResponseRedirect
 
 from booking.models import Booking
 from booking.forms import BookingCreateForm
@@ -42,8 +44,6 @@ class BookingChartView(View):
         return render(request, self.template_name, {'booking_list': booking_list})
 
 
-
-
 class BookingCreateView(FormView, LoginRequiredMixin):
     form_class = BookingCreateForm
     template_name = "booking_add.html"
@@ -54,3 +54,20 @@ class BookingCreateView(FormView, LoginRequiredMixin):
         new_booking.save()
 
         return super(BookingCreateView, self).form_valid(form)
+
+
+class DiggerDeleteView(LoginRequiredMixin, DeleteView):
+    template_name = 'booking_delete.html'
+
+    def get_object(self, queryset=None):
+        return Booking.alive_objects.get(id=self.kwargs['pk'])
+
+    def delete(self, request, *args, **kwargs):
+        if 'cancel' not in request.POST.keys():
+            delete_booking = Booking.alive_objects.get(id=self.kwargs['pk'])
+            delete_booking.is_delete = True
+            delete_booking.delete_date = timezone.now()
+            delete_booking.save()
+
+        # 마이페이지로 redirect하려고 했는데 없어서 임시로 보냄
+        return HttpResponseRedirect('/booking/charts')
